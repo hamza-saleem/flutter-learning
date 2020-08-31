5f766a14b75c02f287b8a52e6eca81455eb08e50
Hamza Saleem <70068443@student.uol.edu.pk>
2020-08-31 13:22:00 +0500

    Learn to add and manage assets etc.

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,16 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("My App"),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Text("Welcome"),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Text('click'),
-        ),
-      ),
+      home: Home(),
     ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Welcome!',
+          style: TextStyle(
+            fontSize: 15.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'PTSerif-Regular',
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.green[700],
+      ),
+      body: Center(
+        // child: Text("Welcome!"),
+        child: Image(image: AssetImage('assets/pic1.jpg')),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('click'),
+        backgroundColor: Colors.green[700],
+      ),
+    );
+  }
+}
