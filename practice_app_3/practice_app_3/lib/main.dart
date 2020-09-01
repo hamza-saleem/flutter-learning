ae8687b0b4576e2811fec33ae1bfee95c17be212
Hamza Saleem <70068443@student.uol.edu.pk>
2020-09-01 18:44:23 +0500

    A simple app for adding and deleting quotes

---
+++ practice_app_3/practice_app_3/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+import 'quote_card.dart';
+
+void main() => runApp(MaterialApp(
+      home: QuoteList(),
+    ));
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes = [
+    Quote(text: "Dont give up! You Fool!", author: "Hamza Saleem"),
+    Quote(text: "Nothing is Impossible!!", author: "Hamza Saleem"),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Awesome Quotes"),
+        backgroundColor: Colors.redAccent,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: quotes
+            .map((quote) => QuoteCard(
+                quote: quote,
+                delete: () {
+                  setState(() {
+                    quotes.remove(quote);
+                  });
+                }))
+            .toList(),
+      ),
+    );
+  }
+}
